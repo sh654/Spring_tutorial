3a2384ab66252d8e104d84d3bf6ae0798622a18f
Your Name <you@example.com>
2024-09-18 18:40:49 +0530

    Savings Minor Updates

---
+++ hibernate-wo-annotations/src/main/java/com/techlabs/HibernateWoAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.techlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateWoAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateWoAnnotationsApplication.class, args);
+	}
+
+}
